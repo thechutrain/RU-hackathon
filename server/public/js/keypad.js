2ce073810262d6d7f1ae140a3693f1e32570e430
thechutrain <chu.chuyalan@gmail.com>
2017-04-23 07:57:32 -0400

    keyboard client js working yo

--- server/public/js/keypad.js
+++ server/public/js/keypad.js
@@ -1,6 +1,32 @@
-// /* global $ */
-// $(document).ready(function () {
-//   $('div.num-button').on('click', function (e) {
-//     console.log($(e).data('num'))
-//   })
-// })
+/* global $ */
+$(document).ready(function () {
+  var phoneNumber = [];
+
+  function updateNumberDisplay () {
+    var displaySelector = $('div#phone-number-display')
+    displaySelector.empty()
+    var h3Number = $('<h3>').text(phoneNumber.join(''))
+    displaySelector.append(h3Number)
+  }
+  // updateNumberDisplay()
+
+  // ========== EVENT LISTENERS ============
+  // event #1: user presses a number button
+  $('div.num-button').on('click', function () {
+    var num = $(this).attr('num')
+    phoneNumber.push(num)
+    updateNumberDisplay()
+  })
+
+  // event #2: user presses a del button
+  $('div#delete-num-button').on('click', function () {
+    phoneNumber.pop()
+    updateNumberDisplay()
+  })
+
+  // event #3: user presses the clear button
+  $('div#clear-num-button').on('click', function () {
+    phoneNumber = []
+    updateNumberDisplay()
+  })
+})
